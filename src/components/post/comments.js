aaf3b0a93def8e98de94fbfb8577d44b04292d4d
Nick Stebb <nickolarse@riseup.net>
2021-07-30 18:40:11 +1200

    Add addcomment component

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { formatDistance } from 'date-fns';
 import { Link } from 'react-router-dom';
 import AddComment from './add-comment';
 
@@ -6,22 +7,31 @@ export default function Comments({ docId, comments: allComments, posted, comment
   const [comments, setComments] = useState(allComments)
 
   return (
-    <div className="pt-1 pt-4 pb-4">
-      {comments.length >= 3 && (
-        <p className="mb-1 text-sm text-gray-500 cursor-pointer">
-          View all {comments.length} comments
+    <>
+      <div className="p-4 pt-1 pb-4">
+        {comments.length >= 3 && (
+          <p className="mb-1 text-sm text-gray-500 cursor-pointer">
+            View all {comments.length} comments
+          </p>
+        )}
+        {comments.slice(0, 3).map((item) => (
+          <p key={`${item.comment}-${item.displayName}`} className="mb-1">
+            <Link to={`/p/${item.displayName}`}>
+              <span className="mr-1 font-bold">{item.displayName}</span>
+            </Link>
+            <span className="ml-1">{item.comment}</span>
+          </p>
+        ))}
+        <p className="mt-2 text-xs text-gray-500 uppercase">
+          {formatDistance(posted, new Date())} ago
         </p>
-      )}
-      {comments.slice(0, 3).map((item) => (
-        <p
-          key={`${item.comment}-${item.displayName}`}
-          className="mb-1">
-          <Link to={`/p/${item.displayName}`}>
-            <span className="mr-1 font-bold">{item.displayName}</span>
-          </Link>
-          <span className="ml-1">{item.comment}</span>
-        </p>
-      ))}
-    </div>
+      </div>
+      <AddComment
+        docId={docId}
+        comments={comments}
+        setComments={setComments}
+        commentInput={commentInput}
+      />
+    </>
   );
 }
