1dfdccfec9a9e1cd15937f1b9209a4ab8cebde53
Nick Stebb <nickolarse@riseup.net>
2021-08-01 18:20:37 +1200

    Pass dispatch through to header in profile props

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,5 +1,8 @@
 import React, { useReducer, useEffect } from 'react';
-import { getUserByUserName, getUserPhotosByUserName } from '../../services/firebase';
+import {
+  getUserByUserName,
+  getUserPhotosByUserName,
+} from '../../services/firebase';
 import Header from './header';
 import Photos from './photos';
 
@@ -7,7 +10,7 @@ const reducer = (state, newState) => ({ ...state, ...newState });
 const initialState = {
   profile: {},
   photosCollection: [],
-  followerCount: 0
+  followerCount: 0,
 }
 
 export default function UserProfile({ username }) {
@@ -20,14 +23,24 @@ export default function UserProfile({ username }) {
     async function getProfileInfoAndPhotos() {
       const [{ ...user }] = await getUserByUserName(username);
       const photos = await getUserPhotosByUserName(username);
-      console.log('user :>> ', photos);
+
+      dispatch({
+        profile: user,
+        photosCollection: photos,
+        followerCount: user.followers.length,
+      })
     };
     getProfileInfoAndPhotos();
   }, [username]);
 
   return (
     <>
-      <Header />
+      <Header
+        photosCollection={photosCollection.length}
+        profile={profile}
+        followerCount={followerCount}
+        setFollowerCount={dispatch}
+      />
       <Photos />
     </>
   )
